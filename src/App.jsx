7cf944992aa911c37ff297e93d0552bdb6ff52ca
enzoulloa <enzo_ulloa.i@gmail.com>
2023-03-28 17:52:35 -0300

    chore: :tada: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import poke from './assets/images/pokedex.jpg'
+function App () {
+  return (
+    <div>
+      <h1>hola</h1>
+      <img className=' bg-red-600' src={poke} alt='' />
+    </div>
+  )
+}
+export default App
