9fce50263bc9705ca5e56f34209523a810fa3612
enzoulloa <enzo_ulloa.i@gmail.com>
2023-04-10 11:13:42 -0300

    feat: :sparkles: add font family

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './routes'
 import './index.css'
+import './fonts.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
