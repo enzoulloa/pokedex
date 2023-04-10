bc461e4a49f531daa7fcefbcb6dbdf4692dc4d8d
enzoulloa <enzo_ulloa.i@gmail.com>
2023-04-10 17:27:50 -0300

    feat: :sparkles: add notFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,19 @@
+import cuatro from '../assets/svg/404.svg'
+import teamRocket from '../assets/images/teamRocket.png'
+import { useNavigate } from 'react-router-dom'
+
 const NotFound = () => {
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate('/')
+  }
   return (
-    <h1>NotFound</h1>
+    <section className=' relative h-[90vh] flex flex-col items-center justify-center'>
+      <img src={cuatro} alt='' className='absolute -z-10 opacity-80' />
+      <img src={teamRocket} alt='' className='' />
+      <p className=' text-2xl my-3 text-center font-medium'>The rocket team <br /> has won this time </p>
+      <button onClick={handleClick} className=' bg-yellow-300 rounded-full py-1 px-4 text-lg font-semibold transition hover:scale-110'>Return</button>
+    </section>
   )
 }
 export default NotFound
