6b5dee448001e2a43c1767c21188cd91c1371b5a
enzoulloa <enzo_ulloa.i@gmail.com>
2023-04-10 12:20:46 -0300

    feat: :lipstick: change shadow styles

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,7 +3,7 @@ import Pokedex from '../assets/images/RotomDex.webp'
 
 const Header = () => {
   return (
-    <header className='fixed top-0 left-0 right-0 flex items-center justify-between py-3 px-3 bg-yellow-300 text-[#111] z-20 shadow-sm'>
+    <header className='fixed top-0 left-0 right-0 flex items-center justify-between py-3 px-3 bg-yellow-300 text-[#111] z-20 shadow-lg'>
       <img className=' w-20 ' src={Pokedex} alt='' />
       <Navbar />
     </header>
