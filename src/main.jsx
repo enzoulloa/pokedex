0966500eb5c3e1e8483972c2228af3f0512a3740
enzoulloa <enzo_ulloa.i@gmail.com>
2023-04-10 12:27:14 -0300

    feat: :sparkles: add react-query config

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Router from './routes'
 import './index.css'
 import './fonts.css'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <Router />
+    </QueryClientProvider>
   </React.StrictMode>
 )
