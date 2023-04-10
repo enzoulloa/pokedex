44ca0fc9fc95376785762e57ce694a70049fc0d6
enzoulloa <enzo_ulloa.i@gmail.com>
2023-04-10 10:57:17 -0300

    feat: :sparkles: add navbar and improve header

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav className='w-[70%] '>
+      <ul className='w-[90%] ml-4 flex items-center justify-between font-semibold'>
+        <li>
+          <NavLink
+            to='/' className=' focus:border-b-2 focus:border-black'
+          >Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/legendaries' className=' focus:border-b-2 focus:border-black'>Legendaries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,8 +1,11 @@
+import Navbar from '../components/Navbar'
+import Pokedex from '../assets/images/RotomDex.webp'
 
 const Header = () => {
   return (
-    <header>
-      header
+    <header className='fixed top-0 left-0 right-0 flex items-center justify-between py-3 px-3 bg-yellow-300 text-[#111] z-20 shadow-sm'>
+      <img className=' w-20 ' src={Pokedex} alt='' />
+      <Navbar />
     </header>
   )
 }
